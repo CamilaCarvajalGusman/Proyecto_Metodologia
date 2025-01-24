3300eff3c837899a44458c0e8b0d8c62250562c9
Camila <camila.carvajal@ucb.edu.bo>
2025-01-24 16:58:28 -0400

    Cambios código fuente 24/01

--- frontend/proyecto_metodologia/src/app/app.module.ts
+++ frontend/proyecto_metodologia/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { DataService } from './data.service';
+
 @NgModule({
   declarations: [
-    AppComponent,
-   
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    
-  ],
-  providers: [provideHttpClient(withInterceptorsFromDi())],
+      ],
+  providers: [
+    provideHttpClient(),
+    DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// [_{{{CITATION{{{_2{Setting up HttpClient • Angular](https://angular.dev/guide/http/setup)
--- frontend/proyecto_metodologia/src/app/data.service.ts
+++ frontend/proyecto_metodologia/src/app/data.service.ts
@@ -7,21 +7,21 @@ import { Observable } from 'rxjs';
 })
 export class DataService {
 
-  private apiUrl = 'http://localhost:3000/usuario';
-  private usuariosUrl = 'http://localhost:3000/usuario'; // URL para la entidad Usuario
-  private empleadoresUrl = 'http://localhost:3000/empleador'; // URL para la entidad Empleador
+  private apiUrl = 'http://localhost:3000/api/data';
+  // private usuariosUrl = 'http://localhost:3000/usuario'; // URL para la entidad Usuario
+  // private empleadoresUrl = 'http://localhost:3000/empleador'; // URL para la entidad Empleador
 
   constructor(private http: HttpClient) { }
 
   getData(): Observable<any> { 
     return this.http.get<any>(this.apiUrl); 
   }
-  getUsuarios(): Observable<any> {
-    return this.http.get(this.usuariosUrl);
-  }
+  // getUsuarios(): Observable<any> {
+  //   return this.http.get(this.usuariosUrl);
+  // }
 
-  getEmpleadores(): Observable<any> {
-    return this.http.get(this.empleadoresUrl);
-  }
+  // getEmpleadores(): Observable<any> {
+  //   return this.http.get(this.empleadoresUrl);
+  // }
 
 }
