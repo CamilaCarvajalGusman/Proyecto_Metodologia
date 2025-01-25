ede38572c0b01b4e1e144ac74211aa65126da377
Camila <camila.carvajal@ucb.edu.bo>
2025-01-25 11:37:36 -0400

    Carrusel de imágenes pag_principal

--- frontend/proyecto_metodologia/src/app/app.module.ts
+++ frontend/proyecto_metodologia/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataService } from './data.service';
+import { HomeComponent } from './home.component';
+import { SelectTypeUserComponent } from './select_type_user.component';
+import { IniciarSesionComponent } from './iniciar_sesion.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-   
+    HomeComponent,
+    SelectTypeUserComponent,
+    IniciarSesionComponent
   ],
   imports: [
     BrowserModule,
-    
+    AppRoutingModule
   ],
-  providers: [provideHttpClient(withInterceptorsFromDi())],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frontend/proyecto_metodologia/src/app/select_type_user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-type-user',
+  template: `
+    <h1>Selecciona el tipo de usuario</h1>
+    <p>Contenido para seleccionar el tipo de usuario...</p>
+  `,
+  styles: [`
+    h1 {
+      color: blue;
+    }
+  `]
+})
+export class SelectTypeUserComponent {
+  
+}
